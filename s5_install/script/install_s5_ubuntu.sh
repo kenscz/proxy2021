cfe56c96623075e26cb5e70ed042eaff0152c1a9
kenscz <1650586976@@qq.com>
2021-05-08 22:54:44 +0800

    更新文档

--- s5_install/script/install_s5_ubuntu.sh
+++ s5_install/script/install_s5_ubuntu.sh
@@ -6,4 +6,4 @@ if [[ -z "$RUN_OPTS" ]];then
     echo "安装失败，缺少必要的参数： --port=xxx --user=xxx --passwd=xxxx"
     exit 1
 fi
-screen -d -m -S s1 ansible s5hosts -m shell -a "wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh $RUN_OPTS" && screen -r s1
+screen -d -m -S s1 ansible s5hosts -m shell -a "wget --no-check-certificate https://raw.github.com/Lozy/danted/master/install.sh -O install.sh && bash install.sh -p $RUN_OPTS" && screen -r s1
